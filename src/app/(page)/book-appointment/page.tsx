6181ea7cf0bb372ed0a3e7fa5337a18f9d396dcf
kashif-lepton <kashif.khan@leptonmaps.com>
2025-02-23 19:37:24 +0530

    FIX

--- src/app/(page)/book-appointment/page.tsx
+++ src/app/(page)/book-appointment/page.tsx
@@ -5,7 +5,7 @@ import Title from "@/components/ui/Title";
 import Glass from "@/lib/helpers";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import React from "react";
-import CountUp, { useCountUp } from "react-countup";
+import CountUp from "react-countup";
 import { ContactForm } from "../../(pages)/lien-he/page";
 
 export default function BookAppointment() {
@@ -203,9 +203,9 @@ export default function BookAppointment() {
         <Glass className="flex flex-col gap-4 px-10">
           <Title title={"Why GTR Immigration?"} />
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  relative z-10 py-2 max-w-7xl mx-auto">
-            {appointment.map((feature, index) => (
+            {/* {appointment.map((feature, index) => (
               <Feature key={feature.title} {...feature} index={index} />
-            ))}
+            ))} */}
           </div>
         </Glass>
       </Container>
@@ -273,48 +273,48 @@ export default function BookAppointment() {
   );
 }
 
-export const appointment = [
-  {
-    id: 1,
-    title: "Government of Canada Licensed",
-    description:
-      "We are authorized to legally represent our clients&apos; files, working directly with the Immigration Department of Canada and the Government of Canada. Our RCIC experts ensure that our clients&apos; files are processed accurately, legally, and securely.",
-    icon: (
-      <Icon
-        icon="material-symbols:person-apron"
-        className="text-6xl w-[50px]"
-      />
-    ),
-  },
-  {
-    id: 2,
-    title: "Proven High Success Rate",
-    description:
-      "This is achieved through a personalized strategy for each of our clients. Each file is analyzed specifically, identifying strengths and weaknesses that need to be addressed. Our wealth of experience helps us anticipate and provide proactive solutions to shorten processing times and ensure success.",
-    icon: (
-      <Icon
-        icon="material-symbols-light:qr-code-2-rounded"
-        className="text-6xl w-[50px]"
-      />
-    ),
-  },
-  {
-    id: 3,
-    title: "Years Of Experience",
-    description:
-      "GreenTech Resource Worldwide Canada is a Canada-based company led by Immigration Expert Mr. Manoj Goswami, the founder and CEO of the company. With an educational background and over 20 years of working experience in Canada, he and his team have a deep understanding of the labor market, culture, and a profound knowledge of Canadian Immigration and Citizenship laws.",
-    icon: <Icon icon="oui:ml-regression-job" className="text-6xl w-[50px]" />,
-  },
-  {
-    id: 4,
-    title: "Reasonable cost guaranteed quality",
-    description:
-      "At GreenTech Resource Worldwide Canada, we offer high-quality immigration services at affordable rates, ensuring your journey towards a new beginning is both accessible and excellent. We have transparent refund policy, emphasizing our commitment to your satisfaction. Your dreams of a new life are well within your reach with GreenTech Resource Worldwide Canada by your side.",
-    icon: (
-      <Icon
-        icon="streamline:quality-education-solid"
-        className="text-6xl w-[50px]"
-      />
-    ),
-  },
-];
+// export const appointment = [
+//   {
+//     id: 1,
+//     title: "Government of Canada Licensed",
+//     description:
+//       "We are authorized to legally represent our clients&apos; files, working directly with the Immigration Department of Canada and the Government of Canada. Our RCIC experts ensure that our clients&apos; files are processed accurately, legally, and securely.",
+//     icon: (
+//       <Icon
+//         icon="material-symbols:person-apron"
+//         className="text-6xl w-[50px]"
+//       />
+//     ),
+//   },
+//   {
+//     id: 2,
+//     title: "Proven High Success Rate",
+//     description:
+//       "This is achieved through a personalized strategy for each of our clients. Each file is analyzed specifically, identifying strengths and weaknesses that need to be addressed. Our wealth of experience helps us anticipate and provide proactive solutions to shorten processing times and ensure success.",
+//     icon: (
+//       <Icon
+//         icon="material-symbols-light:qr-code-2-rounded"
+//         className="text-6xl w-[50px]"
+//       />
+//     ),
+//   },
+//   {
+//     id: 3,
+//     title: "Years Of Experience",
+//     description:
+//       "GreenTech Resource Worldwide Canada is a Canada-based company led by Immigration Expert Mr. Manoj Goswami, the founder and CEO of the company. With an educational background and over 20 years of working experience in Canada, he and his team have a deep understanding of the labor market, culture, and a profound knowledge of Canadian Immigration and Citizenship laws.",
+//     icon: <Icon icon="oui:ml-regression-job" className="text-6xl w-[50px]" />,
+//   },
+//   {
+//     id: 4,
+//     title: "Reasonable cost guaranteed quality",
+//     description:
+//       "At GreenTech Resource Worldwide Canada, we offer high-quality immigration services at affordable rates, ensuring your journey towards a new beginning is both accessible and excellent. We have transparent refund policy, emphasizing our commitment to your satisfaction. Your dreams of a new life are well within your reach with GreenTech Resource Worldwide Canada by your side.",
+//     icon: (
+//       <Icon
+//         icon="streamline:quality-education-solid"
+//         className="text-6xl w-[50px]"
+//       />
+//     ),
+//   },
+// ];
